01b802a072ef263914b7a0d3c6ea1c91ef2cb339
hhugohm <hhugohm@gmail.com>
2018-11-28 13:51:46 -0600

    first commit

---
+++ src/main/java/com/neosuniversity/demoheroku/DemoHerokuApplication.java
@@ -0,0 +1,12 @@
+package com.neosuniversity.demoheroku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoHerokuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoHerokuApplication.class, args);
+	}
+}
